79dcbd1e3c8003d1ea4e8c0820d592ad7b17aa2e
VamosC <3160102243@zju.edu.cn>
2019-05-24 11:17:13 +0800

    Merge branch 'master' of github.com:ATRiiX/Marshmello

--- app/src/main/java/com/atrix/marshmello/HistoryDetect.java
+++ app/src/main/java/com/atrix/marshmello/HistoryDetect.java
@@ -1,8 +1,12 @@
 package com.atrix.marshmello;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class HistoryDetect extends AppCompatActivity {
     @Override
@@ -12,5 +16,28 @@ public class HistoryDetect extends AppCompatActivity {
             getSupportActionBar().hide();
         }
         setContentView(R.layout.activity_history);
+        SharedPreferences storge = getSharedPreferences("data", MODE_PRIVATE);
+        TextView type = (TextView)findViewById(R.id.textView9);
+        TextView tips = (TextView)findViewById(R.id.textView10);
+        String res = storge.getString("type", "未检测");
+        type.setText(res);
+        if(res.equals("中性")) {
+            tips.setText("注意保湿");
+        } else if(res.equals("油性")) {
+            tips.setText("注意去油");
+        } else if(res.equals("干性")) {
+            tips.setText("注意保湿");
+        } else if(res.equals("混合性")) {
+            tips.setText("保持水油平衡");
+        } else {
+            tips.setText("");
+        }
+        ImageButton imageButton = (ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
